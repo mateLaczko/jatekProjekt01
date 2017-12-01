0b28498cb182d77020501268ff7a5b08815814e8
orsitoth <33090934+orsitoth@users.noreply.github.com>
2017-12-01 18:45:51 +0100

    Add files via upload

---
+++ ujjatek/ujjatek/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ujjatek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+             
+        }
+        public List<TextBlock> GetFuckingFields()
+        {
+            List<TextBlock> fieldek = new List<TextBlock>();
+            fieldek.Add(field1);
+            fieldek.Add(field2);
+            fieldek.Add(field3);
+            fieldek.Add(field4);
+            fieldek.Add(field5);
+            fieldek.Add(field6);
+            fieldek.Add(field7);
+            fieldek.Add(field8);
+            fieldek.Add(field9);
+            fieldek.Add(field10);
+            fieldek.Add(field11);
+            fieldek.Add(field12);
+            fieldek.Add(field13);
+            fieldek.Add(field14);
+            fieldek.Add(field15);
+            fieldek.Add(field16);
+            fieldek.Add(field17);
+            fieldek.Add(field18);
+            fieldek.Add(field19);
+            fieldek.Add(field20);
+            fieldek.Add(field21);
+            fieldek.Add(field22);
+            fieldek.Add(field23);
+            fieldek.Add(field24);
+            fieldek.Add(field25);
+            fieldek.Add(field26);
+            fieldek.Add(field27);
+            fieldek.Add(field28);
+            fieldek.Add(field29);
+            fieldek.Add(field30);
+            fieldek.Add(field31);
+            fieldek.Add(field32);
+            fieldek.Add(field33);
+            fieldek.Add(field34);
+            fieldek.Add(field35);
+            fieldek.Add(field36);
+            return fieldek;
+        }
+        private void Window_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            Funkciok OP = new Funkciok();
+            if (e.Key == Key.Right)           
+                OP.JobbraLeptet(GetFuckingFields());          
+            if (e.Key == Key.Left)
+                OP.BalraLeptet(GetFuckingFields());
+            if (e.Key == Key.Up)
+                OP.FelLeptet(GetFuckingFields());
+            if (e.Key == Key.Down)
+                OP.LeLeptet(GetFuckingFields());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            Funkciok OP = new Funkciok();
+            if (button.Name == "Up")
+                OP.FelLeptet(GetFuckingFields());
+            if(button.Name == "Right")
+                OP.JobbraLeptet(GetFuckingFields());
+            if (button.Name == "Down")
+                OP.LeLeptet(GetFuckingFields());
+            if (button.Name == "Left")
+                OP.BalraLeptet(GetFuckingFields());
+        }
+    }
+}
---
+++ ujjatek/ujjatek/osztalyok.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ujjatek
+{
+    public class Funkciok
+    {
+        public void JobbraLeptet(List<TextBlock> fieldek)
+        {
+            int db = 0;
+            for (int i = 0; i < fieldek.Count(); i++)
+            {
+                if (fieldek[i].Text != "" && db < 1 && i != 5 && i != 11 && i != 17 && i != 23 && i != 29 && i != 35)
+                {
+                    fieldek[i + 1].Text = fieldek[i].Text;
+                    fieldek[i].Text = "";
+                    db++;
+                }
+
+            }
+        }
+
+        public void BalraLeptet(List<TextBlock> fieldek)
+        {
+            int db = 0;
+            for (int i = 0; i < fieldek.Count(); i++)
+            {
+                if (fieldek[i].Text != "" && db < 1 && i != 0 && i != 6 && i != 18 && i != 24 && i != 30)
+                {
+                    fieldek[i - 1].Text = fieldek[i].Text;
+                    fieldek[i].Text = "";
+                    db++;
+                }
+
+            }
+        }
+
+        public void FelLeptet(List<TextBlock> fieldek)
+        {
+            int db = 0;
+            for (int i = 0; i < fieldek.Count(); i++)
+            {
+                if (fieldek[i].Text != "" && db < 1 && i != 0 && i != 1 && i != 2 && i != 3 && i != 4 && i != 5)
+                {
+                    fieldek[i - 6].Text = fieldek[i].Text;
+                    fieldek[i].Text = "";
+                    db++;
+                }
+
+            }
+        }
+
+        public void LeLeptet(List<TextBlock> fieldek)
+        {
+            int db = 0;
+            for (int i = 0; i < fieldek.Count(); i++)
+            {
+                if (fieldek[i].Text != "" && db < 1 && i != 30 && i != 31 && i != 32 && i != 33 && i != 34 && i != 35)
+                {
+                    fieldek[i + 6].Text = fieldek[i].Text;
+                    fieldek[i].Text = "";
+                    db++;
+                }
+
+            }
+        }
+    }
+}
